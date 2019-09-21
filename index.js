881acf2b9248ec88141d6caf4d995a552041216b
pavel <paveltkachenko.my@gmail.com>
2019-09-21 13:02:10 +0300

    MeddleMonkey

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
+// ==UserScript==
+// @name Сropio-daily-work
+// @namespace Cropio
+// @version 1.0.0
+// @Author Павел Тккаченко
+// @homepage https://github.com/paveltkachenkomy/cropio-daily-work
+// @description Добавление названий полей в раздел "Работа машин по дням"
+// @downloadURL https://raw.githubusercontent.com/paveltkachenkomy/cropio-daily-work/master/index.js
+// @updateURL https://raw.githubusercontent.com/paveltkachenkomy/cropio-daily-work/master/index.js
+// @include https://cropio.com/machines/reports/daily_work*
+// ==/UserScript==
 $('.task_record_block').prepend('<span></span><br>');
 $('.task_record_block').each(function() {
     var url = $(this).children('a').attr('href');
